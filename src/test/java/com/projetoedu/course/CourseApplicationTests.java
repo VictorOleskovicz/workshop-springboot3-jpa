d044ce722f08ce9d276108032154157d0051c1ff
Victor Oleskovicz <victoroleskovicz1@gmail.com>
2024-02-02 14:49:06 -0300

    Project created

---
+++ src/test/java/com/projetoedu/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoedu.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
