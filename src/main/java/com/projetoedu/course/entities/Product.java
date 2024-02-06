8a4e81d1e236e04e084f16e7791b1edd51dd73bc
Victor Oleskovicz <victoroleskovicz1@gmail.com>
2024-02-06 16:20:22 -0300

    Products

--- src/main/java/com/projetoedu/course/entities/Product.java
+++ src/main/java/com/projetoedu/course/entities/Product.java
@@ -13,8 +13,8 @@ import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 
 @Entity
-@Table(name = "tb_table")
-public class Category implements Serializable{
+@Table(name = "tb_product")
+public class Product implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	
@@ -22,38 +22,73 @@ public class Category implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	private String description;
+	private Double price;
+	private String imgUrl;
 	
 	@Transient
-	private Set<Product> products = new HashSet<>();
+	private Set<Category> categories = new HashSet<>();
 	
-	public Category() {
+	public Product() {
 		
 	}
-	
-	public Category(Long id, String name) {
+
+	public Product(Long id, String name, String description, Double price, String imgUrl) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.imgUrl = imgUrl;
 	}
-
-
 
 	public Long getId() {
 		return id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public Set<Category> getCategories() {
+		return categories;
+	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
 	@Override
 	public int hashCode() {
-		return Objects.hash(id, name);
+		return Objects.hash(id);
 	}
 
 	@Override
@@ -64,15 +99,11 @@ public class Category implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
-		return id == other.id && Objects.equals(name, other.name);
+		Product other = (Product) obj;
+		return Objects.equals(id, other.id);
 	}
 
-	public Set<Product> getProducts() {
-		return products;
-	}
-
-
+	
 	
 	
 	
---
+++ src/main/java/com/projetoedu/course/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.projetoedu.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projetoedu.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
