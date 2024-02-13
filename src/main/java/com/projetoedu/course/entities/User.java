90b6362904cbe567f15ad429c9db8f30d555b42d
Victor Oleskovicz <victoroleskovicz1@gmail.com>
2024-02-13 18:53:09 -0300

    User Update

--- src/main/java/com/projetoedu/course/entities/User.java
+++ src/main/java/com/projetoedu/course/entities/User.java
@@ -88,6 +88,28 @@ public class User implements Serializable {
 		return orders;
 	}
 
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+
 	
 	
 
