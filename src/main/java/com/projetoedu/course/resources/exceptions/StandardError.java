05f3bb2fbe43573bc8aa47a0e7a192a586f65729
Victor Oleskovicz <victoroleskovicz1@gmail.com>
2024-02-13 19:32:58 -0300

    Exception handling - findById

---
+++ src/main/java/com/projetoedu/course/resources/exceptions/StandardError.java
@@ -0,0 +1,60 @@
+package com.projetoedu.course.resources.exceptions;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+public class StandardError implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private Instant timestamp;
+	private Integer status;
+	private String error;
+	private String path;
+	
+	public StandardError() {
+		
+	}
+
+	public StandardError(Instant timestamp, Integer status, String error, String path) {
+		super();
+		this.timestamp = timestamp;
+		this.status = status;
+		this.error = error;
+		this.path = path;
+	}
+
+	public Instant getTimestamp() {
+		return timestamp;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public String getError() {
+		return error;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setTimestamp(Instant timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public void setError(String error) {
+		this.error = error;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+	
+	
+	
+}
