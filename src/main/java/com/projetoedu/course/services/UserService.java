4497fc3ceb2341d1062f01b4e94d81750393fd9f
Victor Oleskovicz <victoroleskovicz1@gmail.com>
2024-02-14 21:05:38 -0300

    Exception handler update

--- src/main/java/com/projetoedu/course/services/UserService.java
+++ src/main/java/com/projetoedu/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.projetoedu.course.repositories.UserRepository;
 import com.projetoedu.course.services.exceptions.DatabaseException;
 import com.projetoedu.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -44,9 +46,16 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
+		try { 
 		User entity = findById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		}
+		
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+
+		}
 	}
 
 	private void updateData(User entity, User obj) {
